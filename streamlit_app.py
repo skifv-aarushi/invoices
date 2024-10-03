9e63a1790effd2dade45931847e21b3979e3d044
skifv-aarushi <aarushijha2505@gmail.com>
2024-10-03 10:06:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,15 @@ import os
 from PIL import Image 
 import google.generativeai as genai
 
-genai.configure(api_key='<api key>'
+genai.configure(api_key='api key')
+
 model=genai.GenerativeModel('gemini-1.5-flash')
 
-def get_gemini_response(Input,Image,propmt):
-    response=model.generate_content((Input,Image[0],input_prompt))
+def get_gemini_response(Input_text,image_data,propmt):
+    response=model.generate_content([Input_text,image_data[0],Input_prompt])
     return response.text
 
-def input_image_details(uploaded_file):
+def Input_image_details(uploaded_file):
     if uploaded_file is not None:
         bytes_data=uploaded_file.getvalue()
         image_parts=[
@@ -30,22 +31,22 @@ st.sidebar.write('Powered by Google Gemini AI')
 st.header('Robobill')
 
 st.subheader('Manage your expenses with Robobill')
-input=st.text_input('What do you want me to do?',key='input')
+Input=st.text_input('What do you want me to do?',key='Input')
 uploaded_file=st.file_uploader('Choose an image',type=['jpg','jpeg','png'])
-image=""
+image=''
 if uploaded_file is not None:
     image=Image.open(uploaded_file)
-st.image(image,caption='uploaded image',use_column_width=True)
+    st.image(image,caption='uploaded image',use_column_width='True')
 submit=st.button("Let's go")
 
-input_prompt="""
+Input_prompt="""
 You are an expert in reading invoices,we are going to upload an image of an invoice and you will have to answer any tupe of questions that the user asks you.
 You have to greet the user first.Make sure to keep the fonts uniform and give the items list in a point-wise format.
 at the end,make sure to repear the name of our app "robobill" and ask the user to use it again.
 """
 if submit:
-    image_data=input_image_details(uploaded_file)
-    response=get_gemini_response(input_prompt,image_data,input)
+    image_data=Input_image_details(uploaded_file)
+    response=get_gemini_response(Input_prompt,image_data,Input)
     st.subheader("Here's what you need to know!")
     st.write(response)
 
